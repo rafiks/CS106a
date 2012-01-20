7f45ac91391c1875322dbc26ad4ed72905ae7465
S <s>
2012-01-20 00:35:43 -0800

    {"type":"TI", "runs":"1"}

--- OperatorMagic.java
+++ OperatorMagic.java
@@ -6,7 +6,7 @@ import acm.program.*;
 
 public class OperatorMagic extends ConsoleProgram {
 	public void run() {
-		println(7 / 3);
-		println(7.0 / 3.0);
+		println(7 / 4);
+		println(7.0 / 4.0);
 	}
 }
