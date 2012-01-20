c1d94bafdfc6a07c9f796a630af3c092dd933f35
S <s>
2012-01-20 14:55:45 -0800

    {"type":"TI", "runs":"3"}

--- OperatorMagic.java
+++ OperatorMagic.java
@@ -6,10 +6,6 @@ import acm.program.*;
 
 public class OperatorMagic extends ConsoleProgram {
 	public void run() {
-		println("This program computes the average of two integers.");
-		int first = readInt("Enter an integer: ");
-		int second = readInt("Enter an integer: ");
-		double average = (double)(first + second) / 2;
-		println("The average is " + average + ".");
+
 	}
 }
