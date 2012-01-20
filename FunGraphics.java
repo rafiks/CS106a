bce7be5863b2f13a3dd57c964e7b4b9456001571
S <s>
2012-01-20 12:16:45 -0800

    {"type":"TI", "runs":"1"}

--- FunGraphics.java
+++ FunGraphics.java
@@ -14,15 +14,15 @@ import java.awt.*;		// Needed for Colors
 
 public class FunGraphics extends GraphicsProgram {
 
-	public void run() {
-		GRect rect = new GRect(10, 20, 100, 200);
-		rect.setFilled(true);
-		add(rect);
-		
+	public void run() {	
 		GOval theOval = new GOval(10, 20, 100, 200);
 		theOval.setFilled(true);
 		theOval.setColor(Color.RED);
 		theOval.setFillColor(Color.GREEN);
 		add(theOval);
+		
+		GRect rect = new GRect(10, 20, 100, 200);
+		rect.setFilled(true);
+		add(rect);
 	}
 }
