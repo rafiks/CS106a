cb750aa622686d97a5e9fda478966e10b1fb876e
S <s>
2012-01-19 23:58:11 -0800

    {"type":"TI", "runs":"2"}

--- HelloProgram.java
+++ HelloProgram.java
@@ -14,7 +14,7 @@ import acm.program.*;
 public class HelloProgram extends GraphicsProgram {
    public void run() {
 	   GLabel label = new GLabel( "hello, world", 100, 75 );
-	   label.setFont("SansSerif-32");
+	   label.setFont("ComicSans-32");
 	   label.setColor(Color.RED);
 	   add(label);
    }
