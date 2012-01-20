302b6a25434bf31764d60028c0db1d874bb6cec0
S <s>
2012-01-20 13:14:16 -0800

    {"type":"TI", "runs":"1"}

--- FunGraphics.java
+++ FunGraphics.java
@@ -15,20 +15,10 @@ import java.awt.*;		// Needed for Colors
 public class FunGraphics extends GraphicsProgram {
 
 	public void run() {
-		GRect rect = new GRect(10, 20, 100, 200);
-		rect.setFilled(true);
-		rect.setColor(Color.DARK_GRAY);
-		add(rect);
-		
 		GOval theOval = new GOval(10, 20, 100, 200);
 		theOval.setFilled(true);
 		theOval.setColor(Color.RED);
 		theOval.setFillColor(Color.GREEN);
 		add(theOval);
-		
-		GLabel bigS = new GLabel("S", 50, 120);
-		bigS.setFont("Courier New-BOLD-36");
-		bigS.setColor(Color.WHITE);
-		add(bigS);
 	}
 }
