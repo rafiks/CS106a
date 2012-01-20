57ad989456970e9ca34bc4604e3ae9faedda5fbd
S <s>
2012-01-20 12:18:25 -0800

    {"type":"TI", "runs":"1"}

--- FunGraphics.java
+++ FunGraphics.java
@@ -25,7 +25,7 @@ public class FunGraphics extends GraphicsProgram {
 		theOval.setFillColor(Color.GREEN);
 		add(theOval);
 		
-		GLabel bigS = new GLabel("S", 10, 120);
+		GLabel bigS = new GLabel("S", 50, 120);
 		bigS.setFont("Courier New-BOLD-36");
 		bigS.setColor(Color.WHITE);
 		add(bigS);
