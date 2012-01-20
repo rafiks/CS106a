9c89a274d0f2b6a640d3cc6386e6ca2e685a55e2
S <s>
2012-01-20 15:49:25 -0800

    {"type":"TI", "runs":"0"}

--- Add2Integers.java
+++ Add2Integers.java
@@ -13,9 +13,9 @@ public class Add2Integers extends ConsoleProgram {
 		int total;
 		int n1;
 		int n2;
+		total = n1 + n2;
 		n1 = readInt("Enter n1: ");
 		n2 = readInt("Enter n2: ");
-		total = n1 + n2;
 		println("The total is " + total + ".");
 	}
 }
