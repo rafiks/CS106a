e5236ca217b105ad8c71f6014f9481fc947586a3
S <s>
2012-01-19 19:47:47 -0800

    {"type":"TI", "runs":"0"}

--- FunGraphics.java
+++ FunGraphics.java
@@ -15,30 +15,7 @@ import java.awt.*;		// Needed for Colors
 public class FunGraphics extends GraphicsProgram {
 
 	public void run() {
-		GLabel label = new GLabel("hello, world", 100, 75);
-		label.setFont("SansSerif-36");
-		label.setColor(Color.RED);
-		add(label);
-
-		GRect rect1 = new GRect(10, 10, 50, 50);
-		add(rect1);
-
-		GRect rect2 = new GRect(300, 75, 200, 100);
-		rect2.setFilled(true);
-		rect2.setColor(Color.RED);
-		add(rect2);
-		
-		GOval oval = new GOval(300, 75, 200, 100);
-		oval.setFilled(true);
-		oval.setFillColor(Color.GREEN);
-		add(oval);
-		
-		double centerY = getHeight() / 2;
-		GLine myFunkyLine = new GLine(100, centerY, 
-									  300, centerY + 50);
-		add(myFunkyLine);
-		
-		GLine dudeWheresMyLine = new GLine(0, 0, 100, 100);
-		// Did not add dudeWheresMyLine to the graphics canvas! 
+		GRect rect = new GRect(10, 20, 100, 200);
+		add(rect);
 	}
 }
