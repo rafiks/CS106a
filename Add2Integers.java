8001500618cd1e5ba0fa85fb52418253b65fde4f
S <s>
2012-01-20 00:29:34 -0800

    {"type":"TI", "runs":"1"}

--- Add2Integers.java
+++ Add2Integers.java
@@ -8,12 +8,14 @@
 import acm.program.*;
 
 public class Add2Integers extends ConsoleProgram {
-   public void run() {
-      println("This program adds two numbers.");
-      int n1 = readInt("Enter n1: ");
-      int n2 = readInt("Enter n2: ");
-      int total = n1 + n2;
-      println("The total is " + total + ".");
-   }
+	public void run() {
+		println(-5 / 2);
+		
+		println("This program adds two numbers.");
+		int n1 = readInt("Enter n1: ");
+		int n2 = readInt("Enter n2: ");
+		int total = n1 + n2;
+		println("The total is " + total + ".");
+	}
 }
 
