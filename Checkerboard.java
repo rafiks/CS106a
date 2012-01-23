ddc712f79dd657022c036b8f1b44ac13ef8c7b37
S <s>
2012-01-22 18:22:51 -0800

    {"type":"TI", "runs":"0"}

--- Checkerboard.java
+++ Checkerboard.java
@@ -7,5 +7,26 @@ import acm.program.*;
 import acm.graphics.*;
 
 public class Checkerboard extends GraphicsProgram {
-
+	private static final int NUM_SQUARES = 8;
+	
+	public void run() {
+		double squareSize = getHeight() / NUM_SQUARES;
+		
+		int index = 0;
+		for (int x = 0; x < NUM_SQUARES; x++) {
+			for (int y = 0; y < NUM_SQUARES; y++) {
+				GRect square = new GRect(x * squareSize, y * squareSize,
+						                 squareSize, squareSize);
+				
+				square.setFilled(true);
+				if (index % 2 == 0) {
+					square.setColor(Color.ORANGE.darker());
+				} else {
+					square.setColor(Color.WHITE);
+				}
+				
+				add(square);
+			}
+		}
+	}
 }
