cdeac690c99ded9a195139cc82b787634402cfe7
S <s>
2012-01-22 18:30:23 -0800

    {"type":"TI", "runs":"0"}

--- AverageTwoNumbers.java
+++ AverageTwoNumbers.java
@@ -6,5 +6,12 @@
 import acm.program.*;
 
 public class AverageTwoNumbers extends ConsoleProgram {
-
+	public void run() {
+		int n1 = readInt("Enter an integer: ");
+		int n2 = readInt("Enter another integer: ");
+		
+		double average = (n1 + n2) / 2;
+		
+		println("The average is " + average);''
+	}
 }
