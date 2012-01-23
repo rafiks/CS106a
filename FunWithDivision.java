8c5e130bdade7af9d0421dbf695a8af68512d4fd
S <s>
2012-01-23 15:44:27 -0800

    {"type":"TI", "runs":"1"}

--- FunWithDivision.java
+++ FunWithDivision.java
@@ -9,7 +9,7 @@ public class FunWithDivision extends ConsoleProgram {
 		int x = 5;
 		int y = 3;
 		
-		double ratio = (double)x / y;
+		double ratio = (int)((double)x / y);
 		
 		println("x / y = " + ratio);
 	}
