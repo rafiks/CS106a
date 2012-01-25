779406135ce178e68ffac823bf34f88e02cead24
S <s>
2012-01-24 17:08:33 -0800

    {"type":"TI", "runs":"0"}

--- Accumulate.java
+++ Accumulate.java
@@ -1,4 +1,22 @@
+/*
+ * File: Accumulate.java
+ * ==========================================
+ * A program that reads and accumulates numbers from the user.
+ * 
+ */
+import acm.program.*;
 
-public class Accumulate {
-
+public class Accumulate extends ConsoleProgram {
+	public void run() {
+		/* Track the total so far. */
+		int total = 0;
+		
+		/* Continuously add new values into the total. */
+		while (true) {
+			int toAdd = readInt("Enter next integer: ");
+			total += toAdd;
+			
+			println("The total so far is " + total);
+		}
+	}
 }
