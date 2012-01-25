c77c4bca5a2020da703168da689606fa83feac27
S <s>
2012-01-24 21:03:18 -0800

    {"type":"TI", "runs":"1"}

--- SeeingStars.java
+++ SeeingStars.java
@@ -23,28 +23,26 @@ public class SeeingStars extends GraphicsProgram {
 	private static final int NUM_STAR_POINTS = 5;
 	
 	private void drawStar(double x, double y, int numSides) {
+		
+	}
+	
+	public void run() {
 		for (int i = 0; i < numSides; i++) {
 			/* Compute the angles of the current point and the next point. */
 			double theta     =      i  * 2 * Math.PI / numSides;
 			double nextTheta = (i + 2) * 2 * Math.PI / numSides;
 			
 			/* Construct a line between those points. */
-			GLine line = new GLine(x + STAR_RADIUS * Math.cos(theta),
-					               y + STAR_RADIUS * Math.sin(theta),
-					               x + STAR_RADIUS * Math.cos(nextTheta),
-								   y + STAR_RADIUS * Math.sin(nextTheta));
+			GLine line = new GLine(STAR_CENTER_X + STAR_RADIUS * Math.cos(theta),
+					               STAR_CENTER_Y + STAR_RADIUS * Math.sin(theta),
+					               STAR_CENTER_X + STAR_RADIUS * Math.cos(nextTheta),
+					               STAR_CENTER_Y + STAR_RADIUS * Math.sin(nextTheta));
 			
 			/* Update the line color. */
 			line.setColor(Color.BLUE);
 			
 			/* Display the line. */
 			add(line);			
-		}
-	}
-	
-	public void run() {
-		for (int i = 5; i < 15; i++) {
-			drawStar(50 + 100 * (i - 5), STAR_CENTER_Y, i);
 		}
 	}
 	
