10d3b5300647efdfda6aaee515903c1ee32402d4
S <s>
2012-01-24 20:26:49 -0800

    {"type":"TI", "runs":"0"}

--- SeeingStars.java
+++ SeeingStars.java
@@ -1,4 +1,23 @@
-
+/*
+ * File: SeeingStars.java
+ * =============================================================
+ * A program that draws pretty stars on the screen!
+ */
 public class SeeingStars {
-
+	/* Constants controlling the location of the star. */
+	private static final double STAR_CENTER_X = 200;
+	private static final double STAR_CENTER_Y = 200;
+	
+	/* Constant controlling the radius of the star. */
+	private static final double STAR_RADIUS = 100;
+	
+	/* Constant controlling how many points the star has. */
+	private static final int NUM_STAR_POINTS = 5;
+	
+	public void run() {
+		for (int i = 0; i < NUM_STAR_POINTS; i++) {
+			double theta = i * Math.PI / NUM_STAR_POINTS;
+		}
+	}
+	
 }
