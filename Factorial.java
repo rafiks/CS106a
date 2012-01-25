ceed66a8d9c54cb9d75919c96f532b77a1968cb2
S <s>
2012-01-24 21:54:14 -0800

    {"type":"TI", "runs":"0"}

--- Factorial.java
+++ Factorial.java
@@ -1,4 +1,17 @@
+/*
+ * File: Factorial.java
+ * ================================================================
+ * A program that shows off computing factorials
+ */
+import acm.program.*;
 
-public class Factorial {
-
+public class Factorial extends ConsoleProgram {
+	public void run() {
+		int result = 1;
+		for (int i = 1; i <= 10; i++) {
+			result *= i;
+		}
+		
+		println("10! = " + result);
+	}
 }
