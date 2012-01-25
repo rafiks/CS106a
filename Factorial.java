022ebaac693ec1f621f56a4da9b3be9b5d7114b7
S <s>
2012-01-24 21:55:45 -0800

    {"type":"TI", "runs":"1"}

--- Factorial.java
+++ Factorial.java
@@ -9,7 +9,7 @@ public class Factorial extends ConsoleProgram {
 	
 	private int factorial(int num) {
 		int result = 1;
-		for (int i = 1; i <= 10; i++) {
+		for (int i = 1; i <= num; i++) {
 			result *= i;
 		}
 		return result;
