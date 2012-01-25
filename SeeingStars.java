16eb9f6027d90968965a4e944e92a512b48cb143
S <s>
2012-01-24 20:28:00 -0800

    {"type":"TI", "runs":"0"}

--- SeeingStars.java
+++ SeeingStars.java
@@ -3,7 +3,11 @@
  * =============================================================
  * A program that draws pretty stars on the screen!
  */
-public class SeeingStars {
+import acm.graphics.*;
+import acm.program.*;
+import java.awt.*;
+
+public class SeeingStars extends GraphicsProgram {
 	/* Constants controlling the location of the star. */
 	private static final double STAR_CENTER_X = 200;
 	private static final double STAR_CENTER_Y = 200;
@@ -16,7 +20,14 @@ public class SeeingStars {
 	
 	public void run() {
 		for (int i = 0; i < NUM_STAR_POINTS; i++) {
-			double theta = i * Math.PI / NUM_STAR_POINTS;
+			double theta     = i * Math.PI / NUM_STAR_POINTS;
+			double nextTheta = (i + 2) * Math.PI / NUM_STAR_POINTS;
+			
+			GLine line = new GLine(STAR_CENTER_X + STAR_RADIUS * Math.cos(theta),
+					               STAR_CENTER_Y + STAR_RADIUS * Math.sin(theta),
+					               STAR_CENTER_X + STAR_RADIUS * Math.cos(nextTheta),
+								   STAR_CENTER_Y + STAR_RADIUS * Math.sin(nextTheta));
+			
 		}
 	}
 	
