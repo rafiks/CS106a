3519513c73616ccc4abbadac2312e3194e4dc961
S <s>
2012-01-26 15:10:10 -0800

    {"type":"TI", "runs":"0"}

--- StickFamily.java
+++ StickFamily.java
@@ -1,3 +1,10 @@
+/*
+ * File: StickFamily.java
+ * =======================================================
+ * A program that draws a family of stick figures!
+ */
+import acm.graphics.*;
+import acm.program.*;
 
 public class StickFamily {
 
