55069d599b66bb06228e7ef6358f5aa9fd71933c
S <s>
2012-01-26 21:14:16 -0800

    {"type":"TI", "runs":"0"}

---
+++ SaturnVLaunch.java
@@ -0,0 +1,74 @@
+/*
+ * File: SaturnVLaunch.java
+ * ========================================================
+ * A program that simulates the launch of a Saturn V moon
+ * rocket.
+ */
+import acm.program.*;
+import acm.graphics.*;
+import acm.util.*;
+
+import java.awt.*;
+
+public class SaturnVLaunch extends GraphicsProgram {
+	private static final double ROCKET_WIDTH = 10;
+	private static final double ROCKET_HEIGHT = 200;
+	
+	private static final double ROCKET_ACCELERATION = -0.1;
+	private static final double PAUSE_TIME = 1000.0 / 24;
+	
+	public void run() {
+		drawBackground();
+		GRect rocket = createRocket();
+		add(rocket);
+		launchRocket(rocket);
+	}
+	
+	private void launchRocket(GRect rocket) {
+		double dy = 0;
+		while (!rocketInOrbit(rocket)) {
+			rocket.move(0, dy);
+			dy += ROCKET_ACCELERATION;
+			
+			pause(PAUSE_TIME);
+		}
+	}
+	
+	private GRect createRocket() {
+		double x = (getWidth() - ROCKET_WIDTH) / 2.0;
+		double y = getHeight() - ROCKET_HEIGHT;
+		GRect result = new GRect(x, y, ROCKET_WIDTH, ROCKET_HEIGHT);
+		result.setFilled(true);
+		result.setColor(Color.GRAY);
+		
+		return result;
+	}
+	
+	private boolean rocketInOrbit(GRect rocket) {
+		return rocket.getY() + rocket.getHeight() < 0;
+	}
+	
+	private void drawStar(double x, double y, double radius, int numPoints) {
+		for (int i = 0; i < numPoints; i++) {
+			/* Compute the angles of the current point and the next point. */
+			double theta     =  i      * 2 * Math.PI / numPoints;
+			double nextTheta = (i + 2) * 2 * Math.PI / numPoints;
+			
+			/* Construct a line between those points. */
+			GLine line = new GLine(x + radius * Math.cos(theta),
+					               y - radius * Math.sin(theta),
+					               x + radius * Math.cos(nextTheta),
+					               y - radius * Math.sin(nextTheta));
+			
+			/* Update the line color. */
+			line.setColor(Color.BLUE);
+			
+			/* Display the line. */
+			add(line);			
+		}
+	}
+	
+	private void drawBackground() {
+		RandomGenerator rgen = RandomGenerator.getInstance();
+	}
+}
