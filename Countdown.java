94e9c3ccee8265e8bf42805305308233eff7177f
S <s>
2012-01-26 20:25:06 -0800

    {"type":"TI", "runs":"0"}

--- Countdown.java
+++ Countdown.java
@@ -1,4 +1,30 @@
+/*
+ * File: Countdown.java
+ * ========================================================
+ * A program that simulates a launch countdown.
+ */
+import acm.program.*;
 
-public class Countdown {
+public class Countdown extends ConsoleProgram {
 
+	private static final int COUNTDOWN_START = 30;
+	private static final int GUIDANCE_START = 15;
+	private static final int IGNITION_START = 9;
+	
+	public void run() {
+		/* Do the launch countdown! */
+		for (int i = COUNTDOWN_START; i > 0; i--) {
+			println("T-" + i + " seconds.");
+			
+			/* Specific mission commands. */
+			if (i == GUIDANCE_START) {
+				println("Guidance is internal.");
+			}
+			if (i == IGNITION_START) {
+				println("Ignition sequence start.");
+			}
+		}
+		
+		println("All engines running. Lift-off!");
+	}
 }
