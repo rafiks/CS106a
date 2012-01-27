1dde2273c247a9c8ab186475088f953302f049bc
S <s>
2012-01-26 22:33:20 -0800

    {"type":"TI", "runs":"1"}

--- RollingDice.java
+++ RollingDice.java
@@ -12,11 +12,10 @@ public class RollingDice extends ConsoleProgram {
 		/* Obtain an instance of the random generator. */
 		RandomGenerator rgen = RandomGenerator.getInstance();
 		
-		/* Roll the dice twice. */
-		int roll1 = rgen.nextInt(1, 6);
-		int roll2 = rgen.nextInt(1, 6);
+		/* Roll the die. */
+		int roll = rgen.nextInt(1, 6);
 		
 		/* Print out the result. */
-		println("We rolled a " + roll1 + " and a " + roll2 + ".");
+		println("We rolled a " + roll + ".");
 	}
 }
