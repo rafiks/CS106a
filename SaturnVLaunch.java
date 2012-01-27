9d87994cb87ec3cde9de92a2b410663cc669589d
S <s>
2012-01-27 11:25:31 -0800

    {"type":"TI", "runs":"1"}

--- SaturnVLaunch.java
+++ SaturnVLaunch.java
@@ -24,7 +24,7 @@ public class SaturnVLaunch extends GraphicsProgram {
 			
 			if (ball.getY() + ball.getHeight() > getHeight()) {
 				ball.move(0, getHeight() - (ball.getY() + ball.getHeight()));
-				dy *= -0.5;
+				dy *= -0.8;
 			}
 			
 			pause(1000.0 / 24);
