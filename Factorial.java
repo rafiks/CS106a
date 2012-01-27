44035f39446eb117e76e36ff5e2210049d89248b
S <s>
2012-01-27 15:33:38 -0800

    {"type":"TI", "runs":"1"}

--- Factorial.java
+++ Factorial.java
@@ -6,19 +6,20 @@
 import acm.program.*;
 
 public class Factorial extends ConsoleProgram {
-	private int factorial(int num) {
-		int result = 1;
-		for (int i = 1; i <= num; i++) {
-			result *= i;
-		}
-		return result;
-	}
-	
+
 	public void run() {
 		int result1 = factorial(10);
 		println("10! = " + result1);
 		
 		int result2 = factorial(13);
 		println("13! = " + result2);
+	}
+	
+	private int factorial(int num) {
+		int result = 1;
+		for (int i = 1; i <= num; i++) {
+			result *= i;
+		}
+		return result;
 	}
 }
