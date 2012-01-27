e0971077ff208e08093adc5405195f3e36f4ab1a
S <s>
2012-01-26 22:32:17 -0800

    {"type":"TI", "runs":"7"}

--- RollingDice.java
+++ RollingDice.java
@@ -5,13 +5,18 @@
  * RandomGenerator.
  */
 import acm.program.*;
-import acm.util.*;
+import acm.util.*;      // <--- Note the new import
 
 public class RollingDice extends ConsoleProgram {
 	public void run() {
+		/* Obtain an instance of the random generator. */
 		RandomGenerator rgen = RandomGenerator.getInstance();
+		
+		/* Roll the dice twice. */
 		int roll1 = rgen.nextInt(1, 6);
 		int roll2 = rgen.nextInt(1, 6);
+		
+		/* Print out the result.
 		println("We rolled a " + roll1 + " and a " + roll2 + ".");
 	}
 }
