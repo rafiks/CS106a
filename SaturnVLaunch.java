bd1880d5a7a57b7827c1866c75414e5bbf5d5b6f
S <s>
2012-01-26 20:32:36 -0800

    {"type":"TI", "runs":"0"}

--- SaturnVLaunch.java
+++ SaturnVLaunch.java
@@ -1,8 +1,13 @@
 /*
- * File: ApolloLaunch.java
+ * File: SaturnVLaunch.java
  * ========================================================
- * A program that simulates the launch of a
+ * A program that simulates the launch of a Saturn V moon
+ * rocket.
  */
+import acm.program.*;
+import acm.graphics.*;
+import java.awt.*;
+
 public class SaturnVLaunch {
 
 }
