6f0b5bb1283d4a86aaa29880d3077b33cb1a9cd6
S <s>
2012-01-27 12:17:01 -0800

    {"type":"TI", "runs":"0"}

--- RetireYoung.java
+++ RetireYoung.java
@@ -1,4 +1,18 @@
+/*
+ * File: RetireYoung.java
+ * =======================================================
+ * A program that will let us retire young (?)
+ */
+import acm.program.*;
 
-public class RetireYoung {
-
+public class RetireYoung extends ConsoleProgram {
+	public void run() {
+		/* Start with only $137 to my name. */
+		int accountBalance = 137;
+		println("I used to have $" + 137);
+		
+		/* Get rich quick with computer science! */
+		getRichQuick(accountBalance);
+		println("Now I have $" + accountBalance);
+	}
 }
