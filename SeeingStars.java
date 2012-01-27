e5f6376b182835ca0a55bb85230e9c210f011a43
S <s>
2012-01-27 15:21:59 -0800

    {"type":"TI", "runs":"1"}

--- SeeingStars.java
+++ SeeingStars.java
@@ -43,7 +43,7 @@ public class SeeingStars extends GraphicsProgram {
 	}
 	
 	public void run() {
-		drawStar(STAR_CENTER_X, STAR_CENTER_Y);
+		drawStar();
 		drawStar(SECOND_STAR_CENTER_X, SECOND_STAR_CENTER_Y);
 	}
 }
