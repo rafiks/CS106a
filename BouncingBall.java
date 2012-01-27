a65e4a6ea9eff094d3fa802ab84f773f998790ed
S <s>
2012-01-27 12:02:13 -0800

    {"type":"TI", "runs":"1"}

--- BouncingBall.java
+++ BouncingBall.java
@@ -48,7 +48,7 @@ public class BouncingBall extends GraphicsProgram {
 		double dx = 5;
 		double dy = 0;
 		double gravity = 1.0;
-		double elasticity = 0.8;
+		double elasticity = 0.9;
 		
 		displayParameters(gravity, elasticity);
 		
