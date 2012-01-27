ba6a75ddc9096f1b950db7cc5b4763f649c83e91
S <s>
2012-01-26 22:31:23 -0800

    {"type":"TI", "runs":"0"}

--- RollingDice.java
+++ RollingDice.java
@@ -1,4 +1,17 @@
+/*
+ * File: RollingDice.java
+ * =======================================================
+ * A program that shows how to roll dice with
+ * RandomGenerator.
+ */
+import acm.program.*;
+import acm.util.*;
 
-public class RollingDice {
-
+public class RollingDice extends ConsoleProgram {
+	public void run() {
+		RandomGenerator rgen = RandomGenerator.getInstance();
+		int roll1 = rgen.nextInt(1, 6);
+		int roll2 = rgen.nextInt(1, 6);
+		println("We rolled a " + roll1 + " and a " + roll2 + ".");
+	}
 }
